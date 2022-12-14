b69f605ef200723ed7a16504e1ef3195ce7f5b6e
DovydasMa <105418683+DovydasMa@users.noreply.github.com>
2022-12-14 02:00:51 +0000

    Update windpressurezones.py

--- Area4/windpressurezones.py
+++ Area4/windpressurezones.py
@@ -135,9 +135,15 @@ def length(line):
             
     lenght = math.sqrt(pow((x[1]-x[0]),2)+pow((y[1]-y[0]),2)+pow((z[1]-z[0]),2))
     return lenght
+
+
+def length_between2_points(x1,y1,z1,x2,y2,z2):
+      
+    lenght = math.sqrt(pow((x1-x2),2)+pow((y1-y2),2)+pow((z1-z2),2))
+    return lenght
   
 
-def wind_pressure_coordinates(line,a,length):
+def new_coordinates_in_line(line,a,length):
     """parses x and y from the given line"""
     cx = [0,0]
     cy = [0,0]
@@ -165,13 +171,168 @@ def wind_pressure_coordinates(line,a,length):
     z[0] = cz[1] - (a / length) * vectorz
     z[1] = cz[0] + (a / length) * vectorz
 
+
+    return x[0],y[0],z[0],x[1],y[1],z[1]
+
+    
+def n_rake_eave_ridge(i):
+    
+    n = 0
+    
+    for j in range(1,line_count+1):   
+  
+        if str("\'" +"l"+str(j)+"\'") in str(data[i]).lower():
+            
+            if data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "EAVE" or data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "RAKE" or data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "RIDGE":
+                n = n + 1
+    
+    return n
+
+"""nuo x2 iki x1"""
+def get_the_vector(x1,y1,z1,x2,y2,z2):
+
+    vectorx = x1 - x2
+    vectory = y1 - y2
+    vectorz = z1 - z2
+
+    return vectorx,vectory,vectorz
+
+def add_the_vectors(vx1,vy1,vz1,vx2,vy2,vz2):
+
+    vectorx = vx1 + vx2
+    vectory = vy1 + vy2
+    vectorz = vz1 + vz2
+    
+    
+    return vectorx,vectory,vectorz
+
+
+
+
+
+
+
+"""problem"""
+n2 = n_rake_eave_ridge(0) *2
+
+cx = [[0]*polygon_count]*n2
+cy = [[0]*polygon_count]*n2
+cz = [[0]*polygon_count]*n2
     
 
-    return x, y, z
+
+i=0
 
 
+    
+p = 0
+
+for j in range(1,line_count+1):
+    
+    
+
+    if str("\'" +"l"+str(j)+"\'") in str(data[i]).lower():
+        
+
+        if data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "EAVE" or data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "RAKE" or data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "RIDGE":
+            
+                line = data[i]["POLYGON"]["@path"]["L"+str(j)]
+                
+                cx[i][p], cy[i][p], cz[i][p], cx[i][p+1], cy[i][p+1], cz[i][p+1] = new_coordinates_in_line(line,a,length(line))
+                
+                p = p + 2
+n3 = n_rake_eave_ridge(0)
+
+cx1 = [[0]*polygon_count]*n3
+cy1 = [[0]*polygon_count]*n3
+cz1 = [[0]*polygon_count]*n3
+co = 0
+                 
+
+"""find 2 closest points"""
+
+i = 0
+    
+p = 0
+
+for j in range(1,line_count+1):
+    
+    
+
+    if str("\'" +"l"+str(j)+"\'") in str(data[i]).lower():
+        
+
+        if data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "EAVE" or data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "RAKE" or data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "RIDGE":
+            
+            line = data[i]["POLYGON"]["@path"]["L"+str(j)]
+
+            x = [0,0]
+            y = [0,0]
+            z = [0,0]
+            count = 0
+            for k in range(1,point_count+1):
+                if str("\'" +"c"+str(k)+"\'") in str(line).lower():
+                    x[count] = line["C"+str(k)]["X"]
+                    y[count] = line["C"+str(k)]["Y"]
+                    z[count] = line["C"+str(k)]["Z"]
+                    count = count + 1
+                    
+            for k in range(0,2):
+                
+                sx1 = cx[i][0]
+                sy1 = cy[i][0]
+                sz1 = cz[i][0]
+
+                sx2 = cx[i][0]
+                sy2 = cy[i][0]
+                sz2 = cz[i][0]
+                for b in range(0,9):
+                    sb1=length_between2_points(sx1,sy1,sz1,x[k],y[k],z[k])
+
+                    sb2=length_between2_points(sx2,sy2,sz2,x[k],y[k],z[k])
+
+                    sk=length_between2_points(cx[i][b],cy[i][b],cz[i][b],x[k],y[k],z[k])
+
+                    if sb1 > sk:
+                        sx1=cx[i][b]
+                        sy1=cy[i][b]
+                        sz1=cz[i][b]
+
+                    elif sb1 > sk:
+                        sx2=cx[i][b]
+                        sy2=cy[i][b]
+                        sz2=cz[i][b]
+
+                vx1,vy1,vz1 = get_the_vector(sx1,sy1,sz1,x[k],y[k],z[k])
+                vx2,vy2,vz2 = get_the_vector(sx2,sy2,sz2,x[k],y[k],z[k])
+                avx,avy,avz = add_the_vectors(vx1,vy1,vz1,vx2,vy2,vz2)
+                """l = math.sqrt(pow(sb1,2)+pow(sb2),2)  (l / length)"""
+
+                
+                if(co % 2) == 0:
+                    cx1[i][co] = x[k] + avx
+                    cy1[i][co] = y[k] + avy
+                    cz1[i][co] = z[k] + avz
+
+                else:
+                    cx1[i][co] = x[k] - avx
+                    cy1[i][co] = y[k] - avy
+                    cz1[i][co] = z[k] - avz
+
+                co = co + 1
+                    
+
+"""
+for i in range(0,n2+1):
+    print("("+str(cx[0][i])+","+str(cy[0][i])+","+str(cz[0][i])+")")
 
 
+for i in range(0,n3+1):
+    print("("+str(cx1[0][i])+","+str(cy1[0][i])+","+str(cz1[0][i])+")")
+"""
+
+
+"""
 
 for i in range(0,polygon_count):
     
@@ -180,13 +341,15 @@ for i in range(0,polygon_count):
         if str("\'" +"l"+str(j)+"\'") in str(data[i]).lower():
             
     
-            if data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "EAVE":
+            if data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "EAVE" or data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "RAKE" or data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] == "RIDGE":
                 
                    line = data[i]["POLYGON"]["@path"]["L"+str(j)]
                    print("xyz wind zone coordinates for F" + str(i) + " Type " +  str(data[i]["POLYGON"]["@path"]["L"+str(j)]["Type"] ))
-                   print(wind_pressure_coordinates(line,a,length(line)))
+                   print(new_coordinates_in_line(line,a,length(line)))
                    
-    
+                   
+                   
 
+"""
 
 
